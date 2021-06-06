d9f5c3a340cbc5da09c8968830e9f721ea8d1711
Yana1112pstr <79226190+Yana1112pstr@users.noreply.github.com>
2021-06-06 09:02:38 +0000

    Deploying to gh-pages from @ Yana1112pstr/ice-cream-project1@6ef08ddf9d9425658a8617a8a6d87376d9686ad0 🚀

--- src.54672969.js
+++ src.54672969.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/ice-cream-project1/src.8743cf8b.js.map
+//# sourceMappingURL=/ice-cream-project1/src.54672969.js.map
